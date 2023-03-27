a32da64834d638147aa7d04fb6df375d4368346d
daniell-olaitan <daniell.olaitan@gmail.com>
2023-03-27 14:40:35 +0100

    Configure an SSH keys using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,14 @@
 # Configure the SSH server to authenticates only through SSH keys
-include stdlib
 
 file_line { 'Turn off passwd auth':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^PasswordAuthentication'
 }
 
 file_line { 'Declare identity file':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
   match  => '^IdentityFile'
