ad7a356cab6f5402bd5f9683615e70d75566a485
daniell-olaitan <daniell.olaitan@gmail.com>
2023-05-10 20:33:16 +0100

    debugged module not documented error

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""
-Exports data from an API to a CSV file
-"""
-
-
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com'
-    todos = requests.get(f"{url}/todos", params={'userId': sys.argv[1]}).json()
-    username = requests.get(f"{url}/users/{sys.argv[1]}").json().get('name')
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(sys.argv[1]), mode='w') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            record = [sys.argv[1], username,
-                      todo.get('completed'), todo.get('title')]
-
-            csv_writer.writerow(record)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
