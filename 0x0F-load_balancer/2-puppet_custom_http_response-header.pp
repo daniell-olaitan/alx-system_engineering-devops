f88ef67561afea641a5b31e8f56d0ae3a61e1629
daniell-olaitan <daniell.olaitan@gmail.com>
2024-01-18 08:19:46 +0100

    Configure Nginx with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,9 +1,8 @@
 # Install Nginx web server with Puppet
-include stdlib
 
-$link = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-$content = "\trewrite ^/redirect_me/$ ${link} permanent;"
-$custom_header = "add_header X-Served-By \$hostname;"
+$config_file = '/etc/nginx/nginx.conf'
+$search = 'http {'
+$config = "${search}\\n\tadd_header X-Served-By"
 
 exec { 'update packages':
   command => '/usr/bin/apt-get update'
@@ -14,35 +13,23 @@ package { 'nginx':
   require => Exec['update packages']
 }
 
-exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
-  require => Package['nginx']
+exec { 'configure firewall':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"'
 }
 
 file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Holberton School',
+  ensure  => file,
+  content => 'Hello World!',
   mode    => '0644',
   owner   => 'root',
   group   => 'root'
 }
 
-file_line { 'Set 301 redirection':
-  ensure   => 'present',
-  after    => 'server_name\ _;',
-  path     => '/etc/nginx/sites-available/default',
-  multiple => true,
-  line     => $content,
-  notify   => Exec['restart nginx'],
-  require  => File['/var/www/html/index.html']
+exec { 'add custom header':
+  command => "/usr/bin/sed -i 's|${search}|${config} \"${hostname}\";|' ${config_file}",
+  unless  => "/usr/bin/grep -q 'X-Served-By' ${config_file}"
 }
 
-file_line { 'Set X-Served-By header':
-  ensure   => 'present',
-  after    => 'http {',
-  path     => '/etc/nginx/nginx.conf',
-  multiple => true,
-  line     => $custom_header,
-  notify   => Exec['restart nginx'],
-  require  => File['/var/www/html/index.html']
+exec { 'restart Nginx':
+  command => '/usr/sbin/service nginx restart'
 }
