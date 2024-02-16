f9346e648629304cd3c30d06048f30d1894eeb08
daniell-olaitan <daniell.olaitan@gmail.com>
2024-02-16 20:18:40 +0100

    Complete the 0th task of the project

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
-function queries a Redit API and returns the number of subscriber
-for a given subredit
+function that queries the Reddit API and return the number of
+subcribers for a given subredit
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers for a given subreddit"""
+    """
+    function that queries the Reddit API and return the number of
+    subcribers for s given subredit
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Advanced-Api:v1.0 (daniell.olaitan@gmail.com)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
